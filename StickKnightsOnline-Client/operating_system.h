832b8052f3926d93944dbb614df611e77af4f1be
Dell_Laptop\Nathanial P. Howard <admin@optimuspi.com>
2018-11-04 12:21:06 -0600

    Update StickKnightsOnline-Client/operating_system.h

--- StickKnightsOnline-Client/operating_system.h
+++ StickKnightsOnline-Client/operating_system.h
@@ -1,3 +1,7 @@
+
+#ifndef _OPERATING_SYSTEM_H__
+#define _OPERATING_SYSTEM_H__
+
 //Make this match OS version
 #define MY_OS 1
 
@@ -5,3 +9,5 @@
 #define WINDOWS_OS 1
 //#define LINUX_OS 2 
 //#define MAC_OS 3
+
+#endif 
